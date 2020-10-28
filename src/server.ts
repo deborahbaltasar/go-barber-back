e306a0fec12350865a3ee0b632e2bef63bcdefa8
deborahbaltasar <deborahbnogueira.dn@gmail.com>
2020-10-28 14:46:34 -0300

    connection with postgres and appointment migration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
+import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
+import 'dotenv/config';
+
+import './database';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('🚀️ Server started on port 3333!');
+  console.log(`🚀️ Server started on port ${PORT}!`);
 });
